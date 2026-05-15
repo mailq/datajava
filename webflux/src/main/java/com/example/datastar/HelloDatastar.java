d1a4265f036e7ee4def2d66f3846df61682ca949
mailq <15148854+mailq@users.noreply.github.com>
2026-05-16 00:34:20 +0200

    Update Spring Webflux to Datastar 1.0

--- webflux/src/main/java/com/example/datastar/HelloDatastar.java
+++ webflux/src/main/java/com/example/datastar/HelloDatastar.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsonp.JSONPModule;
 
 import dev.datastar.Datastar;
 import dev.datastar.WebfluxDatastar;
-import jakarta.json.JsonException;
-import jakarta.json.JsonObject;
 import reactor.core.publisher.Flux;
 
 @RestController
@@ -24,10 +23,10 @@ import reactor.core.publisher.Flux;
 public class HelloDatastar {
 
     @GetMapping("hello-world")
-    public Flux<ServerSentEvent<String>> sayHello(@RequestParam("datastar") String raw) {
+    public Flux<ServerSentEvent<String>> sayHello(@RequestParam("datastar") String raw) throws JsonMappingException, JsonProcessingException {
         var signals = readSignals(raw);
         var hello = "Hello, world!";
-        var delay = signals.getJsonNumber("delay").longValue();
+        var delay = signals.get("delay").longValue();
         return Flux.range(1, hello.length()).map(i -> hello.substring(0, i))
                 .map(text -> "<h1 id=\"message\">%s</h1>".formatted(text))
                 .map(element -> Datastar.patchElements().replace(element))
@@ -35,14 +34,9 @@ public class HelloDatastar {
                 .delayElements(Duration.ofMillis(delay));
     }
 
-    public static JsonObject readSignals(String signals) {
+    public static JsonNode readSignals(String signals) throws JsonMappingException, JsonProcessingException {
         // Needs com.fasterxml.jackson.datatype:jackson-datatype-jakarta-jsonp
-        // and org.glassfish:jakarta.json
-        ObjectMapper objectMapper = JsonMapper.builder().addModule(new JSONPModule()).build();
-        try {
-            return objectMapper.readValue(signals, JsonObject.class);
-        } catch (JsonProcessingException e) {
-            throw new JsonException(e.getMessage(), e);
-        }
+        ObjectMapper objectMapper = JsonMapper.builder().build();
+        return objectMapper.readTree(signals);
     }
 }
